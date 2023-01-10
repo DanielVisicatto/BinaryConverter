25f4d0f6ac17878077913fad455834c8822a1018
Daniel Visicatto <dpfreestyle@gmail.com>
2023-01-10 13:49:27 -0300

    Converts tex to BinaryCode too

--- Program.cs
+++ Program.cs
@@ -1,19 +1,67 @@
 ﻿using BinaryConverter.Entities;
 
-Console.WriteLine("Paste you binary code here...");
-string input = Console.ReadLine().Replace(" ", string.Empty);
-string output = "";
-
-Characters characters = new();
-
-for (int i = 0; i < input.Length; i += 8)
+int userOption = 0;
+while(userOption >= 0 && userOption != 3)
 {
-    string binaryValue = input.Substring(i, 8);
-    char asciiCharacter;
-    if ( characters.binaryCodeDictionary.TryGetValue(binaryValue, out asciiCharacter))
+    Console.WriteLine("\n" + "------------------------------\n" + "Select you option:\n" + "\n" + "1 - Decodify your Binary Code to text\n" + "2 - Codify your text to Binary Code\n" + "3 - Exit Program");
+
+    if (!int.TryParse(Console.ReadLine(), out userOption))
     {
-        output += asciiCharacter;
+        Console.WriteLine("Your input needs to be an valid int number...");
+    }
+    else
+    {
+        string input, output; 
+        Characters characters = new();
+
+        switch (userOption)
+        {
+            case 1:
+                Console.Clear();
+                Console.WriteLine("Enter your binary code here...");
+                input = Console.ReadLine().Replace(" ", string.Empty);
+                output = "";                
+
+                for (int i = 0; i < input.Length; i += 8)
+                {
+                    string binaryValue = input.Substring(i, 8);
+                    char asciiCharacter;
+                    if (characters.binaryCodeDictionary.TryGetValue(binaryValue, out asciiCharacter))
+                    {
+                        output += asciiCharacter;
+                    }
+                }
+                Console.WriteLine("\nYour code returns...\n\n******************************\n\n{0}\n\n******************************",
+                    output);
+                break;
+
+            case 2:
+                Console.Clear();
+                input = "";
+                output = "";
+
+                Console.WriteLine("Enter you text to convert to binary:");
+                input = Console.ReadLine();
+
+                foreach (char c in input)
+                {
+                    string binary = Convert.ToString(c, 2).PadLeft(8, '0');
+                    for (int i = 0; i < binary.Length; i += 8)
+                    {
+                        string binaryValue = binary.Substring(i, 8);
+                        output += binaryValue + " ";
+                    }
+                }
+
+                Console.WriteLine("\nBinary representation of the input string:");
+                Console.WriteLine("\n******************************\n\n{0}\n\n******************************", output);
+
+                break;
+
+            case 3:
+                Console.Clear();
+                Console.WriteLine("------------------------------\n" + "Exiting program...\n" + "------------------------------\n");
+                break;
+        }
     }
 }
-Console.WriteLine("\nYour code returns...\n\n******************************\n\n{0}\n\n******************************",
-    output);
--- README.md
+++ README.md
@@ -7,7 +7,7 @@
 Você precisará do .NET Framework (mais recente) instalado em seu computador
 
 # Descrição do mesmo:
-Este código recebe a ebtrada do usuário, a ser feita em código binário,
+Este código recebe a entrada do usuário, a ser feita em código binário,
 armazena seu conteúdo em uma variável de texto,e compara seu valor aos 
 valores previamente estipulados em um dicionário chave-valor, convertendo
 cada código ao seu caractere correspondente e entregando a frase montada 
@@ -16,7 +16,7 @@ ao usuário.
 ![Example Running](https://github.com/DanielVisicatto/BinaryConverter/blob/master/Img/example.png)
 
 # Melhorias a serem feitas:
- * Colocar a opção de codificar uma frase quando colocada em texto.
+ * Adicionar menu de escolha entre conversão de binário para texto e de texto para binário.
  * Tratar possíveis erros na entrada do usuário.
  * Fazer o FrontEnd.
  
